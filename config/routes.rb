975744631e48fda5bdfeae18cb5a5e860aa65296
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2022-06-07 15:12:02 +0200

    removed the sign up part, and added the authentificate before accessing to the projects editing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :contacts, only: [ :new, :create ]
-  resources :projects, only: [ :show]
+  resources :projects, only: [ :index, :show]
   # get "/", to: "contacts#new"
   # post "/", to: "contacts#create"
 end
