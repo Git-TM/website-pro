9660220b18185a2350d7e005ec36094e6c4d4574
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2022-06-07 15:16:44 +0200

    Added the CRUD operations to projets, and the routes for it

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :contacts, only: [ :new, :create ]
-  resources :projects, only: [ :index, :show]
+  resources :projects
   # get "/", to: "contacts#new"
   # post "/", to: "contacts#create"
 end
