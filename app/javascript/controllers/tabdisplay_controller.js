118ea428ac0542927189eaaf2c826773e389eda9
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2022-10-06 11:41:50 +0200

    removed typed.js and controller for project

--- app/javascript/controllers/tabdisplay_controller.js
+++ app/javascript/controllers/tabdisplay_controller.js
@@ -7,47 +7,30 @@ export default class extends Controller {
  }
 
   change(){
-    let selected = event.currentTarget.getAttribute("href");
-    event.currentTarget.classList.add("selectedtab")
-    this.texttabTargets.forEach(element => {
-      if (element.getAttribute("href") !== selected) {
-        element.classList.remove("selectedtab");
-      }
-    });
-    setTimeout(() => {
-      this.tabprojectTargets.forEach(element => {
-        if (element.classList.contains("active")) {
-          element.classList.remove("d-none");
-          element.classList.add("show");
-        } else if (element.classList.contains("active") == false) {
-          element.classList.add("d-none");
-          element.classList.remove("show");
-        }
-      });
-    }, 100);
+    console.log("That's it");
   }
 }
 
 
-const tabElements = [
-  {
-    id: 'profile',
-    triggerEl: document.querySelector('#profile-tab-example'),
-    targetEl: document.querySelector('#profile-example')
-  },
-  {
-    id: 'dashboard',
-    triggerEl: document.querySelector('#dashboard-tab-example'),
-    targetEl: document.querySelector('#dashboard-example')
-  },
-  {
-    id: 'settings',
-    triggerEl: document.querySelector('#settings-tab-example'),
-    targetEl: document.querySelector('#settings-example')
-  },
-  {
-    id: 'contacts',
-    triggerEl: document.querySelector('#contacts-tab-example'),
-    targetEl: document.querySelector('#contacts-example')
-  }
-];
+// const tabElements = [
+//   {
+//     id: 'profile',
+//     triggerEl: document.querySelector('#profile-tab-example'),
+//     targetEl: document.querySelector('#profile-example')
+//   },
+//   {
+//     id: 'dashboard',
+//     triggerEl: document.querySelector('#dashboard-tab-example'),
+//     targetEl: document.querySelector('#dashboard-example')
+//   },
+//   {
+//     id: 'settings',
+//     triggerEl: document.querySelector('#settings-tab-example'),
+//     targetEl: document.querySelector('#settings-example')
+//   },
+//   {
+//     id: 'contacts',
+//     triggerEl: document.querySelector('#contacts-tab-example'),
+//     targetEl: document.querySelector('#contacts-example')
+//   }
+// ];
