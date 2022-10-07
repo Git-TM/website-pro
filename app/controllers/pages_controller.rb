9ec10940c489238972d62be350c8fc0f107527d2
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2022-10-07 15:51:59 +0200

    Added the iteration for workexperience

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   def home
     @contact = Contact.new
-    @growthprojects = Project.where(category: "Growth / Freelance")
-    @personalprojects = Project.where(category: "Projets personnels")
-    @freelancecodeprojects = Project.where(category: "Freelance Web Dev")
     @projects = Project.all
+    @workexperiences = Workexperience.all.order(:clientname)
   end
 end
